a9306fc7f24ab9ac9083d8529d1a16a810d85135
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-20 01:28:06 +0600

    view engine, load assets

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const app = express()
+const morgan = require('morgan')
+const path = require('path')
 
 dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 3030
 
+
+// middleware method, log request
+app.use(morgan('tiny'))
+
+// parse request to body
+app.use(express.urlencoded({ extended: true }))
+
+// view engine
+app.set("view engine", "ejs")
+// app.set("view", path.resolve(__dirname, "view/ejs"))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
 app.get('/', (req, res) => {
     res.send('User management system')
