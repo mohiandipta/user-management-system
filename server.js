c612566a018a1a26ba29f6383e9e9b3d1d1ef398
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-22 14:45:15 +0600

    database connection error

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const dotenv = require('dotenv')
 const app = express()
 const morgan = require('morgan')
 const path = require('path')
+const connectDB = require('./server/database/connection')
+
 
 dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 3030
 
 
 // middleware method, log request
+app.use(express.json());
 app.use(morgan('tiny'))
+
+//database connection
+connectDB()
 
 // parse request to body
 app.use(express.urlencoded({ extended: true }))
@@ -23,20 +29,6 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
-//index --> new user
-app.get('/add-user', (req, res) => {
-    res.render('add_user')
-})
-
-//index --> new user
-app.get('/update-user', (req, res) => {
-    res.render('update_user')
-})
 
 //load router
 app.use('/', require('./server/routes/router'))
