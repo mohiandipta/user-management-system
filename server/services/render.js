d010f85c55a7d91a8f85b72450925bd525b78818
Mohian Dipta <mohianmostofa@gmail.com>
2021-06-23 13:28:44 +0600

    render/showing user api from mongodb database to the ejs>frontend

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,13 @@
+const axios = require('axios')
+
 exports.homeRoutes = (req, res) => {
-    res.render('index')
+    axios.get('http://localhost:3000/api/users/')
+        .then(function (response) {
+            res.render('index', { users: response.data })
+        })
+        .catch(err => {
+            res.send(err)
+        })
 }
 
 exports.add_user = (req, res) => {
